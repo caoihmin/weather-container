675990047facff6484abf16580d37b4391745ff3
root <root@unassigned-hostname.unassigned-domain>
2017-07-23 15:54:56 -0700

    modified:   setup.sh 	modified:   start.sh 	modified:   terraform.tfstate 	modified:   terraform.tfstate.backup

--- setup.sh
+++ setup.sh
@@ -9,13 +9,14 @@ host_ip="${host_ip##*( )}"
 shopt -u extglob
 host_ip="keng@$host_ip"
 echo $host_ip
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo apt-get update'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo apt-get update'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo apt-get -y install docker.ce'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo chmod +x /usr/local/bin/docker-compose'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'sudo mkdir /etc/hmwk'
-ssh -i /home/keng/.ssh/id_rsa $host_ip 'cd /etc/hmwk; sudo git clone https://github.com/caoihmin/weather-container.git'
+sleep 50
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo apt-get update'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo apt-get update'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo apt-get -y install docker.ce'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)"'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo chmod +x /usr/local/bin/docker-compose'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'sudo mkdir /etc/hmwk'
+ssh -i /home/keng/.ssh/id_rsa $host_ip -oUserKnownHostsFile=/dev/null 'cd /etc/hmwk; sudo git clone https://github.com/caoihmin/weather-container.git'
